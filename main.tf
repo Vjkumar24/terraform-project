f128e1d3d0a0dc017b5967908db02fa8e9150255
Vjkumar24 <138275571+Vjkumar24@users.noreply.github.com>
2023-08-22 10:59:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,6 +87,10 @@ resource "aws_security_group" "five" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "terraform-sg"
+  }
 }
 
 resource "aws_s3_bucket" "six" {
